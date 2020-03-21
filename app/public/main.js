98cddaf839248cceb77270ea4e1e040083c36c94
Agustín Argüello <agus.ap96@gmail.com>
2020-03-21 20:58:46 -0300

    Mock movement buttons on frontend

--- app/public/main.js
+++ app/public/main.js
@@ -3,3 +3,16 @@ let socket = io()
 socket.on('connect', () => {
   document.getElementById('socket_id').innerHTML = socket.id
 })
+
+socket.on('USER_POSITION_CHANGE', user => {
+  console.log(
+    'User',
+    user._id,
+    'changed position to (',
+    user.position.x, user.position.y, ')'
+   )
+})
+
+function userMove(direction) {
+  socket.emit('USER_MOVE_' + direction)
+}
