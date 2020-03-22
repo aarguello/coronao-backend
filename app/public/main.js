8aa65cc2e9551e826d31832eb2048d82496a3eba
Agustín Argüello <agus.ap96@gmail.com>
2020-03-22 16:40:22 -0300

    Console log all received events in frontend demo

--- app/public/main.js
+++ app/public/main.js
@@ -4,22 +4,14 @@ socket.on('connect', () => {
   document.getElementById('socket_id').innerHTML = socket.id
 })
 
-socket.on('USER_POSITION_CHANGE', user => {
-  console.log('USER_POSITION_CHANGE', JSON.stringify(user))
-})
-
-socket.on('USER_APPLY_DAMAGE', user => {
-  console.log('USER_APPLY_DAMAGE', JSON.stringify(user))
-})
-
-socket.on('USER_JOINED', user => {
-  console.log('USER_JOINED', JSON.stringify(user))
-})
-
 function userMove(direction) {
   socket.emit('USER_MOVE_' + direction)
 }
 
 function userAttack() {
   socket.emit('USER_ATTACK')
+}
+
+socket.onevent = function (packet) {
+  console.log(packet.data[0], packet.data[1])
 }
