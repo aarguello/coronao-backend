a8e5de2e472c4be50e51c0f351106e50a1a9e8f9
Agustín Argüello <agus.ap96@gmail.com>
2020-03-22 23:25:36 -0300

    Show position in testing frontend

--- app/public/main.js
+++ app/public/main.js
@@ -12,6 +12,22 @@ function userAttack() {
   socket.emit('USER_ATTACK')
 }
 
+socket.on('USER_POSITION_CHANGE', updatePosition)
+socket.on('USER_JOINED', updatePosition)
+
+const onevent = socket.onevent
+
 socket.onevent = function (packet) {
-  console.log(packet.data[0], packet.data[1])
+  const event = packet.data[0]
+  const data  = packet.data[1]
+  console.log(event, data)
+  onevent.call(this, packet)
+}
+
+function updatePosition(data) {
+  if (data._id === socket.id) {
+    const x = data.position[0]
+    const y = data.position[1]
+    document.getElementById('user_position').innerHTML = `(${x}, ${y})`
+  }
 }
