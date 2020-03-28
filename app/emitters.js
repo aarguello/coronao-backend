010af790f5bd46aa2ecd3ebf42ad47b225c598b9
Agustín Argüello <agus.ap96@gmail.com>
2020-03-28 17:37:15 -0300

    Store this.io in emitters.js

--- app/emitters.js
+++ app/emitters.js
@@ -1,8 +1,5 @@
 
-
-let io
-
-module.exports.setIO = (IO) => io = IO
+module.exports.setIO = (io) => this.io = io
 
 module.exports.userWelcome = (user) => {
 
@@ -15,7 +12,7 @@ module.exports.userWelcome = (user) => {
     inventorySize :global.inventorySize,
   }
 
-  io.to(user._id).emit('USER_WELCOME', { user, globals })
+  this.io.to(user._id).emit('USER_WELCOME', { user, globals })
 }
 
 module.exports.userJoined = (user, socket) => {
@@ -23,85 +20,85 @@ module.exports.userJoined = (user, socket) => {
 }
 
 module.exports.userLeft = (_id) => {
-  io.emit('USER_LEFT', { _id })
+  this.io.emit('USER_LEFT', { _id })
 }
 
 module.exports.userPositionChanged = (_id, position) => {
-  io.emit('USER_POSITION_CHANGED', { _id, position })
+  this.io.emit('USER_POSITION_CHANGED', { _id, position })
 }
 
 module.exports.userDirectionChanged = (_id, direction) => {
-  io.emit('USER_DIRECTION_CHANGED', { _id, direction })
+  this.io.emit('USER_DIRECTION_CHANGED', { _id, direction })
 }
 
 module.exports.userAttacked = (_id, damage) => {
-  io.emit('USER_ATTACKED', { user: { _id }, damage })
+  this.io.emit('USER_ATTACKED', { user: { _id }, damage })
 }
 
 module.exports.userMissedAttack = (_id) => {
-  io.emit('USER_MISSED_ATTACK', { _id })
+  this.io.emit('USER_MISSED_ATTACK', { _id })
 }
 
 module.exports.userReceivedSpell = (_id, spellId) => {
-  io.emit('USER_RECEIVED_SPELL', { user: { _id }, spellId })
+  this.io.emit('USER_RECEIVED_SPELL', { user: { _id }, spellId })
 }
 
 module.exports.userStatChanged = (_id, stat, value) => {
-  io.emit(`USER_STAT_CHANGED`, { _id, stats: { [stat]: value } })
+  this.io.emit(`USER_STAT_CHANGED`, { _id, stats: { [stat]: value } })
 }
 
 module.exports.userVisibilityChanged = (_id, invisible) => {
-  io.emit(`USER_VISIBILITY_CHANGED`, { _id, invisible })
+  this.io.emit(`USER_VISIBILITY_CHANGED`, { _id, invisible })
 }
 
 module.exports.userEquipedItem = (_id, itemId) => {
-  io.emit('USER_EQUIPED_ITEM', { user: { _id }, itemId })
+  this.io.emit('USER_EQUIPED_ITEM', { user: { _id }, itemId })
 }
 
 module.exports.userUnequipedItem = (_id, itemId) => {
-  io.emit('USER_UNEQUIPED_ITEM', { user: { _id }, itemId })
+  this.io.emit('USER_UNEQUIPED_ITEM', { user: { _id }, itemId })
 }
 
 module.exports.userDied = (_id) => {
-  io.emit('USER_DIED', { _id })
+  this.io.emit('USER_DIED', { _id })
 }
 
 module.exports.userRevived = (_id) => {
-  io.emit('USER_REVIVED', { _id })
+  this.io.emit('USER_REVIVED', { _id })
 }
 
 module.exports.userSpoke = (_id, message) => {
-  io.emit('USER_SPOKE', { user: { _id }, message})
+  this.io.emit('USER_SPOKE', { user: { _id }, message})
 }
 
 module.exports.userStartedMeditating = (_id) => {
-  io.emit('USER_STARTED_MEDITATING', { _id })
+  this.io.emit('USER_STARTED_MEDITATING', { _id })
 }
 
 module.exports.userStoppedMeditating = (_id) => {
-  io.emit('USER_STOPPED_MEDITATING', { _id })
+  this.io.emit('USER_STOPPED_MEDITATING', { _id })
 }
 
 module.exports.npcSpawned = (npc) => {
-  io.emit('NPC_SPAWNED', npc)
+  this.io.emit('NPC_SPAWNED', npc)
 }
 
 module.exports.npcDied = (_id) => {
-  io.emit('NPC_DIED', { _id })
+  this.io.emit('NPC_DIED', { _id })
 }
 
 module.exports.npcPositionChanged = (_id, position) => {
-  io.emit('NPC_POSITION_CHANGED', { _id, position })
+  this.io.emit('NPC_POSITION_CHANGED', { _id, position })
 }
 
 module.exports.npcDirectionChanged = (_id, direction) => {
-  io.emit('NPC_DIRECTION_CHANGED', { _id, direction })
+  this.io.emit('NPC_DIRECTION_CHANGED', { _id, direction })
 }
 
 module.exports.npcStatChanged = (_id, stat, value) => {
-  io.emit(`NPC_STAT_CHANGED`, { _id, stats: { [stat]: value } })
+  this.io.emit(`NPC_STAT_CHANGED`, { _id, stats: { [stat]: value } })
 }
 
 module.exports.npcReceivedSpell = (_id, spellId) => {
-  io.emit('NPC_RECEIVED_SPELL', { npc: { _id }, spellId })
+  this.io.emit('NPC_RECEIVED_SPELL', { npc: { _id }, spellId })
 }
