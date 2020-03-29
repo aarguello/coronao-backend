ee53e2cdcba54b50aa146c3d3e52f68e47636fb6
Agustín Argüello <agus.ap96@gmail.com>
2020-03-29 19:24:43 -0300

    Add movement tests on Actor

--- app/model/actor.test.js
+++ app/model/actor.test.js
@@ -1,10 +1,54 @@
 const Actor = require('./actor')
 
 test('Create actor', () => {
+
+  // Arrange
   const actor = new Actor('some random id')
+
+  // Assert
   expect(actor._id).toBe('some random id')
   expect(actor.hp).toBe(0)
   expect(actor.stats.hp.max).toBe(0)
 })
 
+test('it should pivot actor when moving', () => {
 
+  // Arrange
+  const actor = new Actor('')
+  actor.position = [0, 0]
+  global.map = { positions: {}, size: 2 }
+
+  // Act
+  actor.move('RIGHT')
+
+  // Assert
+  expect(actor.direction).toBe('RIGHT')
+})
+
+test('it should move actor when neighbour position is free', () => {
+
+  // Arrange
+  const actor = new Actor('')
+  actor.position = [0, 0]
+  global.map = { positions: {}, size: 2 }
+
+  // Act
+  actor.move('RIGHT')
+
+  // Assert
+  expect(actor.position).toEqual([1, 0])
+})
+
+test('it should not move actor when neighbour position is taken', () => {
+
+  // Arrange
+  const actor = new Actor('')
+  actor.position = [0, 0]
+  global.map = { positions: { '1,0': { USER: 'some other user'} }, size: 2 }
+
+  // Act
+  actor.move('RIGHT')
+
+  // Assert
+  expect(actor.position).toEqual([0, 0])
+})
