06b85a74f0716d29fb7f8bd2e6a2d67fbab4a101
Agustín Argüello <agus.ap96@gmail.com>
2020-05-10 13:14:06 -0300

    startGame(), addPlayer() and removePlayer() methods on GameRoom

--- app/model/game-room.js
+++ app/model/game-room.js
@@ -13,14 +13,28 @@ class GameRoom {
 
   startGame() {
 
+    if (Object.keys(this.players).length < this.capacity) {
+      return
+    }
+
+    this.status = 'INGAME'
   }
 
-  addPlayer(_id, name, race, class_) {
+  addPlayer(_id, player) {
 
+    if (this.status === 'INGAME') {
+      return
+    }
+
+    const position = this.map.randomPosition()
+    player.position = position
+
+    this.map.moveActor(player, null, position)
+    this.players[_id] = player
   }
 
   removePlayer(_id) {
-
+    delete this.players[_id]
   }
 }
 
--- app/model/game-room.test.js
+++ app/model/game-room.test.js
@@ -24,13 +24,90 @@ describe('gameRoom', () => {
 
   describe('startGame', () => {
 
+    it('should not start game with missing players', () => {
+
+      // Arrange
+      const room = new GameRoom('some random id', 1)
+
+      // Act
+      room.startGame()
+
+      // Assert
+      expect(room.status).toBe('QUEUE')
+    })
+
+    it('should start game with full capacity', () => {
+
+      // Arrange
+      const room = new GameRoom('some random id', 1)
+      room.addPlayer('some player id', {})
+
+      // Act
+      room.startGame()
+
+      // Assert
+      expect(room.status).toBe('INGAME')
+    })
+
   })
 
   describe('addPlayer', () => {
 
+    it('should not add player if game started', () => {
+
+      // Arrange
+      const room = new GameRoom('some random id', 1)
+      room.addPlayer('player 1', {})
+      room.startGame()
+
+      // Act
+      room.addPlayer('player 2', {})
+
+      // Assert
+      expect(Object.keys(room.players)).toEqual(['player 1'])
+    })
+
+    it('should add player while in queue', () => {
+
+      // Arrange
+      const room = new GameRoom('some random id', 1)
+
+      // Act
+      room.addPlayer('some player id', {})
+
+      // Assert
+      expect(Object.keys(room.players)).toEqual(['some player id'])
+    })
+
+    it('should set player position', () => {
+
+      // Arrange
+      const room = new GameRoom('some random id', 1)
+      const player = {}
+
+      // Act
+      room.addPlayer('some player id', player)
+
+      // Assert
+      expect(room.map.randomPosition).toHaveBeenCalled()
+      expect(room.map.moveActor).toHaveBeenCalledWith(player, null, player.position)
+    })
   })
 
   describe('removePlayer', () => {
 
+    it('should remove player from room', () => {
+
+      // Arrange
+      const room = new GameRoom('some random id', 2)
+      room.addPlayer('player 1', {})
+      room.addPlayer('player 2', {})
+
+      // Act
+      room.removePlayer('player 1')
+
+      // Assert
+      expect(Object.keys(room.players)).toEqual(['player 2'])
+    })
   })
 })
